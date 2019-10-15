eeb295ff67285b3af34eed05beda247de84a6704
felipe-2705 <felipeaugusto.ferreiradecastro@gmail.com>
2019-10-15 17:18:46 -0300

    interface grafica pronta

--- cliente/GUI.py
+++ cliente/GUI.py
@@ -1,95 +1,56 @@
 import tkinter as tk
+from tkinter import scrolledtext
 
 
-class RoomLogin:
-    roomname = ""
-    password = ""
+
+class option_menu:
     def __init__(self):
         self.window = tk.Tk()
-        self.window.title("Room Login")
-        self.roomlabel = tk.Label(self.window,text ="Room Name:").grid(row=0,column=0)
-        self.passlabel = tk.Label(self.window,text="Password:").grid(row=1,column=0)
-        self.roomentry = tk.Entry(self.window)
-        self.roomentry.grid(row=0,column=1)
-        self.passentry = tk.Entry(self.window)
-        self.passentry.grid(row=1,column=1)
-        self.ok_btn = tk.Button(self.window,text="OK",command= self.gettext).grid(columnspan=2)
-        self.roomname =""
-        self.password= ""
-    def gettext(self):
-        self.roomname =  self.roomentry.get()
-        self.password = self.passentry.get()
-    def windowStart(self):
-        self.window.mainloop()
-    def visible(self,bool):
-        if bool == True:
-            self.window.deiconify()
-        else:
-            self.window.withdraw()
-
-#room._init_()
-
-class joinlogin:
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Join Room")
+        self.window.title("Menu")
         self.roomlabel = tk.Label(self.window,text="Room name").grid(row=0,column=0)
         self.passlabel = tk.Label(self.window,text="Password").grid(row=1,column=0)
         self.roomentry = tk.Entry(self.window)
         self.passentry = tk.Entry(self.window)
-        self.roomentry.grid(row=0,column=1)
-        self.passentry.grid(row=1,column=1)
-        self.join_btn = tk.Button(self.window,text="Join",command=self.gettext).grid(columnspan=2)
-    def gettext(self):
-        self.roomname = self.roomentry.get()
-        self.password = self.passentry.get()
+        self.roomentry.grid(row=0,column=1,columnspan=2)
+        self.passentry.grid(row=1,column=1,columnspan=2)
+        self.create_btn = tk.Button(self.window,text = "Create",command = self.create).grid(row=2,column=0,columnspan=1)
+        self.join_btn = tk.Button(self.window,text="Join",command=self.join).grid(row=2,column=1,columnspan=1)
     def windowStart(self):
         self.window.mainloop()
+    def create(self):
+        print("create")
+        ct = chatRoom()
+    def join(self):
+       print("Join")
+       ct = chatRoom()
     def visible(self,bool):
         if bool == True:
             self.window.deiconify()
         else:
             self.window.withdraw()
-
-class option_menu:
-    roomlogin = RoomLogin()
-    roomlogin.visible(False)
-    wjoin = joinlogin()
-    wjoin.visible(False)
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Menu")
-        self.create_btn = tk.Button(self.window,text = "Create Room",command = self.create).grid(row=0,columnspan=2)
-        self.join_btn = tk.Button(self.window,text="Join Room",command=self.join).grid(row=1,columnspan=2)
-    def windowStart(self):
-        self.window.mainloop()
-    def create(self):
-        self.roomlogin.visible(True)
-        self.wjoin.visible(False)
-    def join(self):
-       self.roomlogin.visible(False)
-       self.wjoin.visible(True)
-       self.chatroom = chatRoom()
-
-
 
 class chatRoom:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("500x700")
-        self.text = tk.scrolledtext.ScrolledText(self.window)
-        self.text.grid(rowspan = 4, columnspan =3)
+        self.window.title('Chat box')
+        self.text = scrolledtext.ScrolledText(self.window,width =60,height=39)
+        self.text.grid(row=0,columnspan=5)
         self.entrada = tk.Entry(self.window)
-        self.entrada.grid(row = 4 , columnspan = 2)
-        self.send_btn = tk.Button(self.window,text ="Send",command = self.sendmsg).grid(row=4,column=2)
+        self.entrada.grid(row=1,column=0,columnspan=3)
+        self.send_btn = tk.Button(self.window,text ="Send",command = self.sendmsg).grid(row=1,column=3,columnspan=2)
     def sendmsg(self):
         mensagem = self.entrada.get()
-        self.text.insert(mensagem)
+        if mensagem != "":
+            self.text.insert(tk.INSERT,mensagem + '\n')
     def visible(self,bool):
         if bool == True:
             self.window.deiconify()
         else:
             self.window.withdraw()
+    def insertMsg(self,Msg):
+        if Msg != "":
+            self.text.insert(tk.Insert,Msg+'\n')
 
 op=option_menu()
 op.windowStart()
